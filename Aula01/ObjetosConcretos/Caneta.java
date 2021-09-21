9411d64a1f9beacf47968acc0b21bc35d3e946ef
T14g0Ferreira <meutrabalho.github@gmail.com>
2021-09-21 01:15:15 -0300

    Cria representação de uma caneta

--- Aula01/ObjetosConcretos/Caneta.java
+++ Aula01/ObjetosConcretos/Caneta.java
@@ -5,11 +5,25 @@ public class Caneta {
     public String cor;
     public boolean tampada;
 
+    public void status(){
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Caneta de cor: " + this.cor);
+        System.out.println("Está tampada? "+ this.tampada);
+    }
+
     public void rabiscar(){
-        if(tampada){
+        if(tampada == true){
             System.out.println("Erro, caneta está tampada");
         } else {
             System.out.println("Rabiscando...");
         }
+    }
+
+    public void tampar(){
+        this.tampada = true;
+    }
+
+    public void destampar(){
+        this.tampada = false;
     }
 }
